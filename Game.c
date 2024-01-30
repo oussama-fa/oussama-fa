c0442d31e92a578bd2bce0368ee6b5212907cb3e
oussama-fa <caaf4040@gmail.com>
2024-01-30 18:01:40 +0100

    New_Game

---
+++ Game.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int number_to_guess, player_guess, attempts = 0;
+    srand(time(NULL));
+
+    number_to_guess = rand() % 100 + 1; // Generates a random number between 1 and 100
+
+    printf("Welcome to the Guess the Number game!\n");
+    printf("I'm thinking of a number between 1 and 100. Try to guess it.\n");
+
+    do {
+        printf("Enter your guess: ");
+        scanf("%d", &player_guess);
+        attempts++;
+
+        if (player_guess < number_to_guess) {
+            printf("Too low! Try again.\n");
+        } else if (player_guess > number_to_guess) {
+            printf("Too high! Try again.\n");
+        } else {
+            printf("Congratulations! You've guessed the number (%d) in %d attempts!\n", number_to_guess, attempts);
+        }
+    } while (player_guess != number_to_guess);
+
+    return 0;
+}
+
